c97692ec300ba1ad2e01918c1a4c8845029955eb
jroscoe5 <36338176+jroscoe5@users.noreply.github.com>
2018-08-26 23:02:05 -0700

    init commit & experience system
    Started on base character class

---
+++ D12/Assets/Classes/BaseCharacter.cs
@@ -0,0 +1,40 @@
+﻿/// BaseCharacter.cs
+/// D12 Team
+/// This class represents a single player and contains their:
+/// - Attributes (TODO)
+/// - Stats (TODO)
+/// - Experience (WIP)
+/// - Traits (TODO)
+/// - Inventory (TODO)
+/// - Multiplayer information (TODO)
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BaseCharacter
+{
+    /// <summary>
+    /// Constructor: Initializes all components to their default state
+    /// </summary>
+    public BaseCharacter ()
+    {
+        experience = new Experience();
+    }
+
+    /// <summary>
+    /// Constructor: Initializes components from a save file
+    /// </summary>
+    /// <param name="savefile">Path to save file</param>
+    public BaseCharacter (string savefile)
+    {
+        // TODO
+    }
+
+    /// <summary>
+    /// Internal experience manager
+    /// </summary>
+    private Experience experience;
+
+
+}
---
+++ D12/Assets/Classes/Experience.cs
@@ -0,0 +1,55 @@
+﻿/// Experience.cs
+/// D12 team
+/// <summary>
+/// Experience management system for a character
+/// </summary>
+public class Experience
+{
+    /// <summary>
+    /// Default starting point value
+    /// </summary>
+    public readonly int STARTING_EXP_VAL = 24;
+
+    /// <summary>
+    /// Constructor: Initializes experience points to default value
+    /// </summary>
+    public Experience()
+    {
+        Points = STARTING_EXP_VAL;
+    }
+
+    /// <summary>
+    /// Constructor: Initializes experience points to parameter
+    /// </summary>
+    /// <param name="initialValue">Starting experience points</param>
+    public Experience (int initialValue)
+    {
+        Points = initialValue;
+    }
+
+    /// <summary>
+    /// Deducts from current point balance if possible
+    /// </summary>
+    /// <param name="val">Deduction value</param>
+    /// <returns>If deduction was successful</returns>
+    public bool Deduct(int val)
+    {
+        if (val < 0 || val > Points) return false;
+        Points -= val;
+        return true;
+    }
+
+    /// <summary>
+    /// Adds to current point balance
+    /// </summary>
+    /// <param name="val">Addition value</param>
+    public void Add (int val)
+    {
+        Points += val;
+    }
+
+    /// <summary>
+    /// Current experience points character holds
+    /// </summary>
+    public int Points { get; set; }
+}
