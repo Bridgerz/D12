0ba4cba746f3b2b5d210781030f9295646d86ad6
jroscoe5 <36338176+jroscoe5@users.noreply.github.com>
2018-09-07 15:23:13 -0700

    SpellTrait completed, Action/Effect next

--- D12/Assets/Classes/Traits/SpellTraits/SpellTrait.cs
+++ D12/Assets/Classes/Traits/SpellTraits/SpellTrait.cs
@@ -43,26 +43,44 @@ class SpellTrait : Trait
     /// <summary>
     /// Internal number of spell targets
     /// </summary>
-    private int targets;
+    private int numTargets;
 
     /// <summary>
     /// Number of spell targets
     /// </summary>
-    public int Targets
+    public int NumTargets
     {
         get
         {
-            return targets;
+            return numTargets;
         }
     }
 
-
-    public SpellTrait(string name, int cost, int manaCost, int range, int targets, bool active) : base(name, cost, active)
+    /// <summary>
+    /// Constructor: Initializes all data members of a spell trait
+    /// </summary>
+    /// <param name="name">Name of spell trait</param>
+    /// <param name="cost">EXP cost of spell trait</param>
+    /// <param name="manaCost">Mana cose of spell trait</param>
+    /// <param name="range">Spell's range in meters</param>
+    /// <param name="numTargets">Number of targets spell affects</param>
+    /// <param name="active">Flag indicating if trait has been purchased (unlocked)</param>
+    public SpellTrait(string name, int cost, int manaCost, int range, int numTargets, bool active) : base(name, cost, active)
     {
         this.manaCost = manaCost;
         this.range = range;
-        this.targets = targets;
+        this.numTargets = numTargets;
     }
 
+    /// <summary>
+    /// Simulates a spell cast on a number of targets
+    /// </summary>
+    /// <param name="badTargets">Targets intended to be damaged by the spell</param>
+    /// <param name="goodTargets">Targets inteded to be buffed/healed by the spell</param>
+    /// <returns>If spell was successfully cast</returns>
+    public virtual bool Cast(BaseCharacter[] badTargets = null, BaseCharacter[] goodTargets = null)
+    {
+        return false;
+    }
 }
 
