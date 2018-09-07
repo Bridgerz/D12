be5a7dfa4ab758a683958cf98e0c412be8b028fb
jroscoe5 <36338176+jroscoe5@users.noreply.github.com>
2018-09-06 22:16:52 -0700

    Traits tree framework started

---
+++ D12/Assets/Classes/Traits/SpellTraits/SpellTrait.cs
@@ -0,0 +1,68 @@
+﻿/// SpellTrait.cs
+/// D12 Team
+
+using System.Collections.Generic;
+
+/// <summary>
+/// Traits that are cast as spells
+/// </summary>
+class SpellTrait : Trait
+{
+    /// <summary>
+    /// Internal mana cost of spell
+    /// </summary>
+    private int manaCost;
+
+    /// <summary>
+    /// Mana cost of spell
+    /// </summary>
+    public int ManaCost
+    {
+        get
+        {
+            return manaCost;
+        }
+    }
+
+    /// <summary>
+    /// Internal range in meters of the spell
+    /// </summary>
+    private int range;
+
+    /// <summary>
+    /// Range in meters of the spell
+    /// </summary>
+    public int Range
+    {
+        get
+        {
+            return range;
+        }
+    }
+
+    /// <summary>
+    /// Internal number of spell targets
+    /// </summary>
+    private int targets;
+
+    /// <summary>
+    /// Number of spell targets
+    /// </summary>
+    public int Targets
+    {
+        get
+        {
+            return targets;
+        }
+    }
+
+
+    public SpellTrait(string name, int cost, int manaCost, int range, int targets, bool active) : base(name, cost, active)
+    {
+        this.manaCost = manaCost;
+        this.range = range;
+        this.targets = targets;
+    }
+
+}
+
---
+++ D12/Assets/Classes/Traits/Trait.cs
@@ -0,0 +1,85 @@
+﻿/// Trait.cs
+/// D12 Team
+
+using System;
+using System.Collections.Generic;
+
+/// <summary>
+/// Parent class for all traits
+/// </summary>
+public class Trait
+{
+    /// <summary>
+    /// Internal name of the trait
+    /// </summary>
+    protected string name;
+
+    /// <summary>
+    /// Internal EXP cost to puchase trait
+    /// </summary>
+    protected int cost;
+
+    /// <summary>
+    /// Internal flag if trait has been purchased and is now active
+    /// </summary>
+    protected bool active;
+
+    /// <summary>
+    /// Name of the trait
+    /// </summary>
+    public string Name
+    {
+        get
+        {
+            return name;
+        }
+    }
+
+    /// <summary>
+    /// EXP cost to purchase (unlock) trait
+    /// </summary>
+    public int Cost
+    {
+        get
+        {
+            return cost;
+        }
+    }
+
+    /// <summary>
+    /// Flag if trait has been unlocked
+    /// </summary>
+    public bool Active
+    {
+        get
+        {
+            return active;
+        }
+    }
+
+    /// <summary>
+    /// List of parent traits (traits directly above it in the tree)
+    /// </summary>
+    public List<Trait> parents;
+
+    /// <summary>
+    /// List of children traits (traits directly beneath it in the tree)
+    /// </summary>
+    public List<Trait> children;
+
+    /// <summary>
+    /// Constructor: Initializes a Trait with a name, cost and active flag
+    /// </summary>
+    /// <param name="name">Name of trait</param>
+    /// <param name="cost">EXP cost to purchase (unlock) trait</param>
+    /// <param name="active">Flag indicating trait's lock/unlock status</param>
+    public Trait(string name, int cost, bool active)
+    {
+        children = new List<Trait>();
+        parents = new List<Trait>();
+        this.active = active;
+        this.cost = cost;
+        this.name = name;
+    }
+}
+
