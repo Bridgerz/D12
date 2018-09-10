707feaa8865bc273f5bef2139d74ee896264b273
jroscoe5 <36338176+jroscoe5@users.noreply.github.com>
2018-09-09 17:47:48 -0700

    Stats started

--- D12/Assets/Classes/Traits/SpellTraits/Trait_Firebolt.cs
+++ D12/Assets/Classes/Traits/SpellTraits/Trait_Firebolt.cs
@@ -1,5 +1,6 @@
 ﻿/// Trait_Firebolt.cs
 /// D12 Team
+/// Firebolt spell traits
 
 using System;
 
@@ -17,8 +18,32 @@ public class Firebolt1 : SpellTrait
     {
         msg = desc;
 
-        if (badTargets.Length != 0) return false;
+        if (badTargets.Length != 1) return false;
 
         // damage the badTarget here (no concept of hp yet hmm something to do)
+
+        return true;
+    }
+}
+
+/// <summary>
+/// Spell Trait: Firebolt 2
+/// </summary>
+public class Firebolt2 : SpellTrait
+{
+    public Firebolt2(bool active) : base("Firebolt 2", "sling hissing fire at a target, dealing a hit of fire damage", 1, 1, 80, 1, active)
+    {
+
+    }
+
+    public override bool Cast(out string msg, BaseCharacter[] badTargets = null, BaseCharacter[] goodTargets = null, object[] additionalObjects = null)
+    {
+        msg = desc;
+
+        if (badTargets.Length != 1) return false;
+
+        // damage the badTarget here (no concept of hp yet hmm something to do)
+
+        return true;
     }
 }
