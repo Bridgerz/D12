da29ca82834b8a0264739b08048e9099fa7a6102
jroscoe5 <36338176+jroscoe5@users.noreply.github.com>
2018-09-12 20:40:46 -0700

    Backwards progress on traits

--- D12/Assets/Classes/Traits/SpellTraits/Trait_LayOnHands.cs
+++ D12/Assets/Classes/Traits/SpellTraits/Trait_LayOnHands.cs
@@ -8,7 +8,7 @@ using System.Collections.Generic;
 /// </summary>
 public class LayOnHands1 : SpellTrait
 {
-    public LayOnHands1(bool active) : base("Lay on Hands 1", "touch, heal a target for 1 hit of damage", 1, 1, 1, 1, active) { }
+    public LayOnHands1(bool active) : base("Lay on Hands 1", "touch, heal a target for 1-2 hits of damage", 1, 1, 1, 1, active) { }
 
     public override bool Cast(out string msg, int roll, List<BaseCharacter> badTargets = null, List<BaseCharacter> goodTargets = null, List<object> additionalObjects = null)
     {
@@ -23,7 +23,7 @@ public class LayOnHands1 : SpellTrait
         var target = goodTargets[0];
 
         // Heal the target
-        var regen = (roll == 12) ? 2 : 1;
+        var regen = (roll >= 12) ? 2 : 1;
         target.Stats.Modify(HP: regen);
 
         // Return message and success
@@ -37,7 +37,119 @@ public class LayOnHands1 : SpellTrait
 /// </summary>
 public class LayOnHands2 : SpellTrait
 {
-    public LayOnHands2(bool active) : base("Lay on Hands 1", "touch, heal a target for 1 hit of damage", 1, 1, 1, 1, active) { }
+    public LayOnHands2(bool active) : base("Lay on Hands 2", "touch, heal a target for 1-2 hits of damage", 1, 1, 1, 1, active) { }
+
+
+    public override bool Cast(out string msg, int roll, List<BaseCharacter> badTargets = null, List<BaseCharacter> goodTargets = null, List<object> additionalObjects = null)
+    {
+        // Is there a target to heal? Range checking will be done by a higher up class to account for range buffs
+        if (goodTargets.Count != 1)
+        {
+            msg = "Invalid targets";
+            return false;
+        }
+
+        // The spell's target BaseCharacter
+        var target = goodTargets[0];
+
+        // Heal the target
+        var regen = (roll >= 10) ? 2 : 1;
+        target.Stats.Modify(HP: regen);
+
+        // Return message and success
+        msg = "Healed " + target.Info.Name + " for " + regen + " HP.";
+        return true;
+    }
 }
 
-// More to add
+/// <summary>
+/// Spell Trait: Lay On Hands 3
+/// </summary>
+public class LayOnHands3 : SpellTrait
+{
+    public LayOnHands3(bool active) : base("Lay on Hands 3", "touch, heal a target for 2-3 hits of damage", 1, 1, 1, 1, active) { }
+
+
+    public override bool Cast(out string msg, int roll, List<BaseCharacter> badTargets = null, List<BaseCharacter> goodTargets = null, List<object> additionalObjects = null)
+    {
+        // Is there a target to heal? Range checking will be done by a higher up class to account for range buffs
+        if (goodTargets.Count != 1)
+        {
+            msg = "Invalid targets";
+            return false;
+        }
+
+        // The spell's target BaseCharacter
+        var target = goodTargets[0];
+
+        // Heal the target
+        var regen = (roll >= 12) ? 3 : 2;
+        target.Stats.Modify(HP: regen);
+
+        // Return message and success
+        msg = "Healed " + target.Info.Name + " for " + regen + " HP.";
+        return true;
+    }
+}
+
+
+/// <summary>
+/// Spell Trait: Lay On Hands 4
+/// </summary>
+public class LayOnHands4 : SpellTrait
+{
+    public LayOnHands4(bool active) : base("Lay on Hands 4", "touch, heal a target for 2-3 hits of damage", 1, 1, 1, 1, active) { }
+
+
+    public override bool Cast(out string msg, int roll, List<BaseCharacter> badTargets = null, List<BaseCharacter> goodTargets = null, List<object> additionalObjects = null)
+    {
+        // Is there a target to heal? Range checking will be done by a higher up class to account for range buffs
+        if (goodTargets.Count != 1)
+        {
+            msg = "Invalid targets";
+            return false;
+        }
+
+        // The spell's target BaseCharacter
+        var target = goodTargets[0];
+
+        // Heal the target
+        var regen = (roll >= 8) ? 3 : 2;
+        target.Stats.Modify(HP: regen);
+
+        // Return message and success
+        msg = "Healed " + target.Info.Name + " for " + regen + " HP.";
+        return true;
+    }
+}
+
+
+/// <summary>
+/// Spell Trait: Lay On Hands 5
+/// </summary>
+public class LayOnHands5 : SpellTrait
+{
+    public LayOnHands5(bool active) : base("Lay on Hands 5", "touch, heal a target for 3 hits of damage", 1, 1, 1, 1, active) { }
+
+
+    public override bool Cast(out string msg, int roll, List<BaseCharacter> badTargets = null, List<BaseCharacter> goodTargets = null, List<object> additionalObjects = null)
+    {
+        // Is there a target to heal? Range checking will be done by a higher up class to account for range buffs
+        if (goodTargets.Count != 1)
+        {
+            msg = "Invalid targets";
+            return false;
+        }
+
+        // The spell's target BaseCharacter
+        var target = goodTargets[0];
+
+        // Heal the target
+        var regen = 3;
+        target.Stats.Modify(HP: regen);
+
+        // Return message and success
+        msg = "Healed " + target.Info.Name + " for " + regen + " HP.";
+        return true;
+    }
+}
