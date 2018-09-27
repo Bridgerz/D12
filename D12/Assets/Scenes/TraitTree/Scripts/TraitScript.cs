2a4506780fd9691222b8bbac1f83e5862e932107
jroscoe5 <36338176+jroscoe5@users.noreply.github.com>
2018-09-26 23:40:55 -0700

    Trait Scene going well, some scaling bugs

--- D12/Assets/Scenes/TraitTree/Scripts/TraitScript.cs
+++ D12/Assets/Scenes/TraitTree/Scripts/TraitScript.cs
@@ -5,25 +5,38 @@ using System.Collections;
 using System.Collections.Generic;
 using Assets.Classes.Traits;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class TraitScript : MonoBehaviour
 {
     public Trait trait;
 
+    public Text nameBox, descBox, costBox, activeBox;
+
     // Use this for initialization
     void Start()
     {
-
     }
 
     // Update is called once per frame
     void Update()
     {
-
     }
 
     public void OnClick()
     {
-        Debug.Log(trait.Name);
+        nameBox.text = trait.Name;
+        descBox.text = trait.Desc;
+        costBox.text = "EXP COST: " + trait.Cost.ToString();
+        if(trait.Active)
+        {
+            activeBox.text = "ACTIVE";
+            activeBox.color = Color.green;
+        }
+        else
+        {
+            activeBox.text = "LOCKED";
+            activeBox.color = Color.red;
+        }
     }
 }
--- D12/Assets/Scenes/TraitTree/Scripts/TraitTreeDriver.cs
+++ D12/Assets/Scenes/TraitTree/Scripts/TraitTreeDriver.cs
@@ -15,8 +15,22 @@ public class TraitTreeDriver : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
+        // will need to modify this when saving is implemented
         character = new BaseCharacter();
-	}
+
+        // link up all the gameobjects and traits
+        var tree = character.TraitTrees.AccumenTree;
+
+        ling1.GetComponent<TraitScript>().trait = tree.linguist1;
+        ling2.GetComponent<TraitScript>().trait = tree.linguist2;
+        ling3.GetComponent<TraitScript>().trait = tree.linguist3;
+        ling4.GetComponent<TraitScript>().trait = tree.linguist4;
+        ling5.GetComponent<TraitScript>().trait = tree.linguist5;
+        multilinguist.GetComponent<TraitScript>().trait = tree.multilinguist;
+        transcriber.GetComponent<TraitScript>().trait = tree.transcriber;
+        archlinguist.GetComponent<TraitScript>().trait = tree.archlinguist;
+        globalLinguist.GetComponent<TraitScript>().trait = tree.globalLocal;
+    }
 	
 	// Update is called once per frame
 	void Update () {
