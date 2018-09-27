34d6c9e7cae08fb889a7da17ce38b5c98eba455d
Bridgerz <bridgerzoske@gmail.com>
2018-09-26 21:40:22 -0700

    Added Curio slots

---
+++ D12/Assets/Inventory/Equipment/Scripts/EquipSlot.cs
@@ -0,0 +1,84 @@
+﻿using Assets.Inventory.Scripts.Item.ItemModels;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class EquipSlot : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler, IPointerClickHandler
+{
+    public EquipManager Manager;
+    public GameObject Item;
+    public bool Occupied;
+    public ItemType SlotType;
+
+    public void Start()
+    {
+        Manager = GameObject.Find("Equipment Panel").GetComponent<EquipManager>();
+    }
+
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        if (ItemOm.SelectedItem == null)
+        {
+            if (Occupied)
+            {
+                ItemOm.SelectedItem = Item;
+                Item.transform.SetParent(GameObject.Find("DragParent").transform);
+                ItemOm.IsDragging = true;
+                Occupied = false;
+                transform.GetComponent<Image>().color = SlotColorHighlights.Green;
+            }
+            else
+            {
+                return;
+            }
+        }
+        else
+        {
+            if (!Occupied)
+            {
+                Manager.Equip(ItemOm.SelectedItem, gameObject);
+            }
+            else
+            {
+                //Manager.Swap(ItemOm.SelectedItem, gameObject);
+            }
+        }
+    }
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        if (ItemOm.SelectedItem == null)
+        {
+            return;
+        }
+        if (ItemOm.SelectedItem.GetComponent<ItemOm>().Item.Type != SlotType)
+        {
+            transform.GetComponent<Image>().color = SlotColorHighlights.Red;
+        }
+        else if (Occupied)
+        {
+            transform.GetComponent<Image>().color = SlotColorHighlights.Yellow;
+        }
+        else
+        {
+            transform.GetComponent<Image>().color = SlotColorHighlights.Green;
+        }
+    }
+
+    public void Equip(GameObject item)
+    {
+        Item = item;
+    }
+
+    public void Drop()
+    {
+
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        transform.GetComponent<Image>().color = SlotColorHighlights.Blue;
+    }
+
+
+}
