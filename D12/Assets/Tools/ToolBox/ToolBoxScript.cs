b3739081994cad1daa0ee5bb3ac2b8909050bea7
Bridgerz <bridgerzoske@gmail.com>
2018-10-22 22:29:54 -0700

    Updated a lot of things not dealing with tooltip update but im puting it here any way :)

---
+++ D12/Assets/Tools/ToolBox/ToolBoxScript.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ToolBoxScript : MonoBehaviour {
+    public List<GameObject> Tools;
+    public GameObject ListPanel;
+
+	// Use this for initialization
+	void Start () {
+		// initialize all Tools;
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ D12/Assets/Tools/ToolBox/ToolBoxToggle.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ToolBoxToggle : MonoBehaviour {
+    public bool Visible;
+    public GameObject ToolBox;
+
+	void Start () {
+        Visible = true;
+	}
+    
+    public void ToggleToolBox()
+    {
+        if (Visible)
+        {
+            ToolBox.SetActive(false);
+            Visible = false;
+        }
+        else
+        {
+            ToolBox.SetActive(true);
+            Visible = true;
+        }
+    }
+
+}
